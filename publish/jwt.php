e843cf47832794ea9edbd95f01f1335bc063e4a5
冷斌 <xykxyk2008@163.com>
2020-08-19 15:37:55 +0800

    jwt 重写

--- publish/jwt.php
+++ publish/jwt.php
@@ -1,11 +1,13 @@
 <?php
 return [
-    'alg' => 'HMACSHA256',
-    'key' => 'ice',
+    'alg'    => 'HMACSHA256',
+    'key'    => 'ice',
     // jwt 过期时间
-    'exp' => 2 * 3600,
+    'exp'    => 2 * 3600,
     // 刷新token 过期时间
-    'ttl' => 24 * 3600 * 30,
+    'ttl'    => 24 * 3600 * 30,
     // 是否 单点登录
-    'sso' => false
+    'sso'    => false,
+    //如果开启 单点登录，请设置 key
+    'ssoKey' => 'id',
 ];
